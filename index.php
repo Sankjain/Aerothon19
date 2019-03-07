10529e26218452a0d3a15272dd9c5cd4d6492634
Sankjain <sanketh.jain98@gmail.com>
2019-03-07 11:39:03 +0530

    Updated 2nd Page

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>AirBus</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Conference project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<!-- Menu -->
+
+	<div class="menu trans_500">
+		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+			<div class="menu_close_container"><div class="menu_close"></div></div>
+			<div class="logo menu_logo">
+				<a href="#">
+					<div class="logo_container d-flex flex-row align-items-start justify-content-start">
+						<div class="logo_image"><div><img src="images/logo.jpg" alt=""></div></div>
+						<div class="logo_content">
+							<div class="logo_text logo_text_not_ie"></div>
+							<div class="logo_sub"></div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<ul>
+				<li class="menu_item"><a href="index.html">Home</a></li>
+				<li class="menu_item"><a href="#">About us</a></li>
+				<li class="menu_item"><a href="#">Flights</a></li>
+				<li class="menu_item"><a href="#">Overview</a></li>
+				
+				<li class="menu_item"><a href="contact.html">Contact</a></li>
+			</ul>
+		</div>
+	
+	</div>
+	
+	<!-- Home -->
+
+	<div class="home">
+		<!-- <div class="home_background" style="background-image: url(images/index.jpg)"></div> -->
+		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/index.jpg" data-speed="0.8"></div>
+
+		<!-- Header -->
+
+		<header class="header" id="header">
+			<div>
+				<div class="header_top">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<div class="header_top_content d-flex flex-row align-items-center justify-content-start">
+									<div>
+										<a href="#">
+											<div class="logo_container d-flex flex-row align-items-start justify-content-start">
+												<div class="logo_image"><div><img src="images/logo.png" alt=""></div></div>
+												<div class="logo_content">
+													<div id="logo_text" class="logo_text logo_text_not_ie">Air Buzz</div>
+													<div class="logo_sub">The Ultimate Travel</div>
+												</div>
+											</div>
+										</a>	
+									</div>
+									
+									<div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header_nav" id="header_nav_pin">
+					<div class="header_nav_inner">
+						<div class="header_nav_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="header_nav_content d-flex flex-row align-items-center justify-content-start">
+											<nav class="main_nav">
+												<ul>
+													<li class="active"><a href="index.html">Home</a></li>
+													<li><a href="#">About Us</a></li>
+													<li><a href="speakers.html">Flights</a></li>
+													<li><a href="events.html">Overview</a></li>
+													
+													<li><a href="contact.html">Contact</a></li>
+												</ul>
+											</nav>
+											<div class="header_extra ml-auto">
+												<div class="header_search"><i class="fa fa-search" aria-hidden="true"></i></div>
+												<div class="button header_button"><a href="#">Search and Filter</a></div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="search_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="search_content d-flex flex-row align-items-center justify-content-end">
+											<form action="#" id="search_container_form" class="search_container_form">
+												<input type="text" class="search_container_input" placeholder="Search" required="required">
+												<button class="search_container_button"><i class="fa fa-search" aria-hidden="true"></i></button>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>
+		</header>
+
+		<div class="home_content_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="home_content">
+							<div class="home_date"></div>
+							<div class="home_title">AirBus</div>
+							
+							<div class="home_text">Airbus is an international reference in the aerospace sector. We design, manufacture and deliver industry-leading commercial aircraft, helicopters, military transports, satellites and launch vehicles, as well as providing data services, navigation, secure communications, urban mobility and other solutions for customers on a global scale.</div>
+							<div class="home_buttons">
+								<div class="button home_button"><a href="#">Flights</a></div>
+								<div class="button home_button"><a href="#">Overview</a></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Intro -->
+
+	<div class="intro">
+		<div class="intro_content d-flex flex-row flex-wrap align-items-start justify-content-between">
+
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_1.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#popup1">FLight A310</a></div>
+					<div class="intro_subtitle"></div>
+				</div>
+				</div>
+			
+
+
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_2.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#">Flight A320</a></div>
+					<div class="intro_subtitle">
+			</div>
+			</div>
+			</div>
+			
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_3.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#">Flight A330</a></div>
+					<div class="intro_subtitle"></div>
+				</div>
+			</div>
+
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_4.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#">Flight A340</a></div>
+					<div class="intro_subtitle"></div>
+				</div>
+			</div>
+
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_5.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#">Flight A350</a></div>
+					<div class="intro_subtitle"></div>
+				</div>
+			</div>
+
+			<!-- Intro Item -->
+			<div class="intro_item">
+				<div class="intro_image"><img src="images/intro_6.jpg" alt=""></div>
+				<div class="intro_body">
+					<div class="intro_title"><a href="#">Support and Help</a></div>
+					<div class="intro_subtitle">.</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	
+	
+
+
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="footer_content">
+			<div class="container">
+				<div class="row">
+					
+					<!-- Footer Column -->
+					<div class="col-lg-4 footer_col">
+						<div class="footer_about">
+							<div>
+								<a href="#">
+									<div class="logo_container d-flex flex-row align-items-start justify-content-start">
+										<div class="logo_image"><div><img src="images/logo.png" alt=""></div></div>
+										<div class="logo_content">
+											<div id="logo_text" class="logo_text logo_text_not_ie">AirBus</div>
+											<div class="logo_sub">Airbus is an international reference in the aerospace sector. We design, manufacture and deliver industry-leading commercial aircraft, helicopters, military transports, satellites and launch vehicles, as well as providing data services, navigation, secure communications, urban mobility and other solutions for customers on a global scale.</div>
+										</div>
+									</div>
+								</a>	
+							</div>
+							<div class="footer_about_text">
+								<p></p>
+							</div>
+						</div>
+					</div>
+
+					<!-- Footer Column -->
+					<div class="col-lg-3 footer_col">
+						<div class="footer_links">
+							<ul>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Speakers</a></li>
+								<li><a href="#">Event Dates</a></li>
+								<li><a href="#">Information</a></li>
+								<li><a href="#">Calendar</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- Footer Column -->
+					<div class="col-lg-3 footer_col">
+						<div class="footer_links">
+							<ul>
+								<li><a href="#">Logistics</a></li>
+								<li><a href="#">Our Partners</a></li>
+								<li><a href="#">Testimonials</a></li>
+								<li><a href="#">Price Plans</a></li>
+								<li><a href="#">News</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- Footer Column -->
+					<div class="col-lg-2 footer_col">
+						<div class="footer_links">
+							<ul>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Speakers</a></li>
+								<li><a href="#">Event Dates</a></li>
+								<li><a href="#">Information</a></li>
+								<li><a href="#">Calendar</a></li>
+							</ul>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="footer_extra">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="footer_extra_content d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-lg-start justify-content-center">
+							
+							<div class="footer_extra_right ml-lg-auto text-lg-right">
+								<div class="footer_extra_links">
+									<ul>
+										<li><a href="contact.html">Contact us</a></li>
+										<li><a href="#">Sitemap</a></li>
+										<li><a href="#">Privacy</a></li>
+									</ul>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+		
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="styles/bootstrap4/popper.js"></script>
+<script src="styles/bootstrap4/bootstrap.min.js"></script>
+<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
